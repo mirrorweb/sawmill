bcf5592a7f8d8a4231a1e3bef1e5046eec40b75c
Lee Booth <lee.booth@mirrorweb.com>
2019-06-13 15:30:06 +0100

    Added ProgressBar class to utils. Functional - room for improvement.

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,10 @@
+from time import sleep
 from typing import List
 import unittest
 import logging
 
 from sawmill.logger import Sawmill
+from sawmill.utils import ProgressBar
 
 __all__: List = ['SawmillTests']
 
@@ -13,4 +15,21 @@ class SawmillTests(unittest.TestCase):
     def test_one(self):
         self.logger.debug("test output")
         self.assertTrue(isinstance(self.logger, logging.Logger))
+
+    def test_prog_bar(self):
+        items = list(range(0, 43))
+        
+        with ProgressBar.load(len(items), msg='Testing progress bar...', draw_mode='') as pb:
+            for item in items:
+                # Do stuff...
+                sleep(0.1)
+                # Update Progress Bar
+                pb.update()
+
+        with ProgressBar.load(len(items), msg='Testing pacman progress bar...', draw_mode='pacman') as pb:
+            for item in items:
+                # Do stuff...
+                sleep(0.1)
+                # Update Progress Bar
+                pb.update()
 
